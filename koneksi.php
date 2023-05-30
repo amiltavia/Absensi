f47a71da3787ec987200a1c93f5bfbd02bd79ab4
amiltavia <134992513+amiltavia@users.noreply.github.com>
2023-05-30 11:36:26 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+  
+$dbHost = 'localhost';
+$dbName = 'absenkaryawan'; //ubah dengan nama database Anda
+$dbUsername = 'root';
+$dbPassword = '';
+
+
+$mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+?>
